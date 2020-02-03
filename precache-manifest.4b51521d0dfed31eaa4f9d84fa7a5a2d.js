5e7d2e3ff02fa2e34626bc143de0394c0bad41d1
Mr Sekh Afrid <abhijeetmurmu97@gmail.com>
2020-02-03 23:11:54 +0530

    Updates

---
+++ precache-manifest.4b51521d0dfed31eaa4f9d84fa7a5a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0bc08a2ed32c7cc6fe69f52fa9c24e",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "68a4fd75d64ba90d0060",
+    "url": "/first-react-app/static/css/main.28556ccc.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/first-react-app/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-react-app/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a4fd75d64ba90d0060",
+    "url": "/first-react-app/static/js/main.5dcf7e63.chunk.js"
+  },
+  {
+    "revision": "26fdb0f83a2f304e241e",
+    "url": "/first-react-app/static/js/runtime-main.f68c7fa8.js"
+  }
+]);
